d769e0f7fdac10655ab1cfc1fb86e7939ab1febf
Durrell Jules <dujules23@gmail.com>
2020-11-08 20:49:00 -0500

    completed prototypes

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,5 +1,23 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
+class CuboidMaker
+{
+    constructor (length, width, height)
+    {
+    this.length = length;
+    this.width = width;
+    this.height = height;
+    }
+
+    speak()
+    {
+        
+    }
+}
+
+
+
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
